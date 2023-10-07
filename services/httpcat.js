73e7cf5b473389fc8849d686a1879442c1a956fa
Vinh <37470201+TinLite@users.noreply.github.com>
2023-10-07 09:14:25 +0700

    Update httpcat

--- services/httpcat.js
+++ services/httpcat.js
@@ -1,6 +1,6 @@
 const throwError = (res, code) => {
     res.status(code)
-    res.send("<!DOCTYPE html><body style=\"background-color: black;\"><img src=\"https://http.cat/400.jpg\"></body></html>")
+    res.send(`<!DOCTYPE html><body style="background-color: black;"><img src="https://http.cat/${code}.jpg"></body></html>`)
 }
 
 module.exports = throwError
